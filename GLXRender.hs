02f758d6921133d1e606b16b6e7d754bac7ac04e
Aren Olson <reacocard@gmail.com>
2011-04-07 23:59:10 -0500

    code cleanup; bring tfp path up to software level

--- GLXRender.hs
+++ GLXRender.hs
@@ -27,12 +27,12 @@ attributes = [
                glxDoubleBuffer          
              , glxRGBA                  
 --             , glxStencilSize, 2
-             , glxRedSize               , 1
-             , glxGreenSize             , 1
-             , glxBlueSize              , 1
-             , glxAlphaSize             , 1
+             , glxRedSize               , 8
+             , glxGreenSize             , 8
+             , glxBlueSize              , 8
+             , glxAlphaSize             , 8
 --             , glxDepthSize             , 24
---             , glxBindToTextureRgbaExt  
+             , glxBindToTextureRgbaExt  
              ]
 
 data GLXRenderEngine = GLXRenderEngine { 
@@ -74,10 +74,6 @@ checkImage (TextureSize2D w h) n f  =  [ f c |
 withX11Image (TextureSize2D width height) image act = do
     pixels <- mapM (\y -> mapM (\x -> getPixel image x y) [0..(fromIntegral $ width-1)]) [0..(fromIntegral $ height-1)]
     colors <- return $ map (\pix -> Color4 
---              (fromIntegral (shiftR pix 16) :: Int)
---              (fromIntegral ((shiftR pix 8) .&. 255) :: Int)
---              (fromIntegral (pix .&. 255) :: Int)
---              (255 :: Int)
                 (fromIntegral (shiftR pix 16) :: GLubyte)
                 (fromIntegral ((shiftR pix 8) .&. 255) :: GLubyte)
                 (fromIntegral (pix .&. 255) :: GLubyte )
@@ -103,12 +99,6 @@ initRender dpy screen compwin = do
                     [textureName] <- GL.genObjectNames  1
                     GL.textureBinding GL.Texture2D $= Just textureName
                     GL.textureFilter GL.Texture2D $= ((GL.Nearest, Nothing), GL.Nearest)
-
-                    --withCheckImage checkImageSize 0x08 (\c -> Color4 c c c 255) $
-                    --    GL.texImage2D Nothing NoProxy 0  RGBA' checkImageSize 0
-
-
-                    --GL.ortho2D (-500) 500 (-500) 500
                     GL.ortho2D (0) 1680 (0) 1050
                     return $ Just $ GLXRenderEngine { glxr_window=compwin, glxr_visual=vis }
 
@@ -132,27 +122,12 @@ paintAll dpy render winlist = do
 
 tfpPaintAll dpy render winlist = do 
     GL.clear [GL.ColorBuffer]
-
-    -- draw the quad
---  GL.color $ color3f 0 1 1
---  GL.renderPrimitive GL.Quads $ do
---      GL.vertex $ vertex2f (1500,600)
---      GL.texCoord $ texCoord2f (0,100)
---      GL.vertex $ vertex2f (1600,600)
---      GL.texCoord $ texCoord2f (100,100)
---      GL.vertex $ vertex2f (1600,500)
---      GL.texCoord $ texCoord2f (100,0)
---      GL.vertex $ vertex2f (1500,500)
---      GL.texCoord $ texCoord2f (0,0)
-
-
     _winlist <- mapM (updateWinPixmap dpy) winlist
     _winlist <- mapM (updateWinGLPixmap dpy render) _winlist
-    mapM_ (\(w,o) -> tfpPaintWin dpy render w o) $ zip _winlist $ map (*10) [-10..10]
+    mapM_ (tfpPaintWin dpy render) _winlist
     glXSwapBuffers dpy $ glxr_window render
     --sync dpy False
     return _winlist
-
 
 updateWinPixmap :: Display -> Win -> IO Win
 updateWinPixmap dpy win = 
@@ -180,61 +155,46 @@ updateWinGLPixmap dpy render win =
     where
         xvi     = glxr_visual render        
 
-
---paintWin :: Display -> GLXRenderEngine -> Win -> IO ()
-tfpPaintWin dpy render win offset =
+tfpPaintWin :: Display -> GLXRenderEngine -> Win -> IO ()
+tfpPaintWin dpy render win = do
     case win_glpixmap win of
         Nothing -> return ()
         Just glPixmap -> do
+            let window = win_window win
+            attrs <- getWindowAttributes dpy window
+            let width = (wa_width attrs)
+                height = (wa_height attrs)
+                xpos = wa_x attrs
+                ypos = 1050 - (wa_y attrs)
+
             glXBindTexImageEXT dpy glPixmap glxFrontLeftExt []
-
-            let n1 = 100 + offset
-                n2 = (-100) + offset
-
             GL.color $ color3f 1 1 1
             GL.renderPrimitive GL.Quads $ do
-                GL.vertex $ vertex2f (n2,n1)
+                GL.vertex $ vertex2f (fromIntegral $ xpos,fromIntegral $ ypos-height)
                 GL.texCoord $ texCoord2f (0,0)
-                GL.vertex $ vertex2f (n1,n1)
+                GL.vertex $ vertex2f (fromIntegral $ xpos,fromIntegral $ ypos)
                 GL.texCoord $ texCoord2f (1,0)
-                GL.vertex $ vertex2f (n1,n2)
+                GL.vertex $ vertex2f (fromIntegral $ xpos+width,fromIntegral $ ypos)
                 GL.texCoord $ texCoord2f (1,1)
-                GL.vertex $ vertex2f (n2,n2)
+                GL.vertex $ vertex2f (fromIntegral $ xpos+width,fromIntegral $ ypos-height)
                 GL.texCoord $ texCoord2f (0,1)
-
-
 
             glXReleaseTexImageEXT dpy glPixmap glxFrontLeftExt
 
 
-
-
 softwarePaintAll dpy render winlist = do 
     GL.clear [GL.ColorBuffer]
-    screen      <- return $ defaultScreen dpy
-    rootwin     <- rootWindow dpy screen
-
-    mapM_ (\(w,o) -> softwarePaintWin dpy render w o) $ zip winlist $ map (*20) [0..20]
-    --softwarePaintWin dpy render rootwin 0
+    mapM_ (softwarePaintWin dpy render) winlist
     glXSwapBuffers dpy $ glxr_window render
     --sync dpy False
     return winlist
-
-
-
     
-softwarePaintWin dpy render win offset = do
-    print "PAINTWIN"
+softwarePaintWin dpy render win = do
     let window = win_window win
-    --let window = win
     attrs <- getWindowAttributes dpy window
     let width = (wa_width attrs)
         height = (wa_height attrs)
-
-    print $ (show width) ++ " " ++ (show height)
-
     if (width < 0) || (height < 0) then return () else do
-
         mImage <- catch (do
             -- FIXME: figure out what the right value for the planes mask is - currently 2^24 - 1
             image <- getImage dpy window 0 0 (fromIntegral width) (fromIntegral height) 16777215 xyPixmap 
@@ -243,21 +203,11 @@ softwarePaintWin dpy render win offset = do
         case mImage of
             Nothing -> return ()
             Just image -> do
-
-                writeImage ("windows/" ++ (show window) ++ ".ppm") image width height
-
-                --pixels <- mapM (\y -> mapM (\x -> getPixel image x y) [0..(width-1)]) [0..(height-1)]
-                --colors <- return $ map (\pix -> Color4 (shiftR pix 16) ((shiftR pix 8) .&. 255) (pix .&. 255) 128) $ concat pixels
-
+                --writeImage ("windows/" ++ (show window) ++ ".ppm") image width height
                 let size = TextureSize2D (fromIntegral width) (fromIntegral height)
-                --let size = TextureSize2D 32 32
                 withX11Image size image $ GL.texImage2D Nothing NoProxy 0  RGBA' size 0
-
-
                 let xpos = wa_x attrs
                     ypos = 1050 - (wa_y attrs)
-
-                print $ (show xpos) ++ "  " ++ (show ypos)
 
                 GL.color $ color3f 1 1 1
                 GL.renderPrimitive GL.Quads $ do
@@ -270,44 +220,7 @@ softwarePaintWin dpy render win offset = do
                     GL.vertex $ vertex2f (fromIntegral $ xpos+width,fromIntegral $ ypos-height)
                     GL.texCoord $ texCoord2f (0,1)
 
-
-                --[textureName] <- GL.genObjectNames  1
-                --GL.textureBinding GL.Texture2D $= Just textureName
-                
-                --let size = TextureSize2D 4 4
-                --pixels <- return ([[0,250,0,250],[0,250,0,250],[0,250,0,250],[0,250,0,250]] :: [[Int]])
-                --colors <- return $ map (\i -> Color4 i i i 255) $ concat pixels
-                --colors <- return $ map (\pix -> Color4 (shiftR pix 16) ((shiftR pix 8) .&. 255) (pix .&. 255) 255) $ concat pixels
-                --colors <- return $ checkImage size 0x08 (\c -> Color4 c c c (255 :: Int))
-                --withArray colors $
-                --    (GL.texImage2D Nothing NoProxy 0  RGBA' size 0) . PixelData RGBA UnsignedByte
-
-                --let size = TextureSize2D 65 24
-                --withCheckImage size 0x08 (\c -> Color4 255 150 70 (255::GLubyte)) $
-                --    GL.texImage2D Nothing NoProxy 0  RGBA' size 0
-
-                --withTestImage size 0x08 (\c -> Color4 (255::GLubyte) (150::GLubyte) (70::GLubyte) (0::GLubyte)) $
-                --    GL.texImage2D Nothing NoProxy 0  RGBA' size 0
-
---              let n1 = 400 + offset
---                  n2 = 200 + offset
-
---              GL.color $ color3f 1 1 1
---              GL.renderPrimitive GL.Quads $ do
---                  GL.vertex $ vertex2f (n2,n1)
---                  GL.texCoord $ texCoord2f (0,0)
---                  GL.vertex $ vertex2f (n1,n1)
---                  GL.texCoord $ texCoord2f (1,0)
---                  GL.vertex $ vertex2f (n1,n2)
---                  GL.texCoord $ texCoord2f (1,1)
---                  GL.vertex $ vertex2f (n2,n2)
---                  GL.texCoord $ texCoord2f (0,1)
-
                 return ()
-
-
-
-
 
 writeImage filename image width height = do
     let header = "P3 " ++ (show width) ++ " " ++ (show height) ++ " 255" ++ "\n"
